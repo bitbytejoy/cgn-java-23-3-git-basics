56b32e267e6bdf9870f32ef0728983e8114e6054
bitbytejoy <bitbytejoy@users.noreply.github.com>
2023-09-19 14:48:02 +0200

    Update Main.java
    Third-party change demo

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -2,6 +2,6 @@ package de.neuefische;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello blabla!");
+        System.out.println("Hello from GitHub!");
     }
 }
