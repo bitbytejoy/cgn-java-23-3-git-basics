77bf5596e4d25628594cf142fef66b4e2a7b8329
bitbytejoy <bitbytejoy@gmail.com>
2023-09-19 14:41:58 +0200

    Hello message changed

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -2,6 +2,6 @@ package de.neuefische;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello cgn-java-23-3!");
     }
 }
